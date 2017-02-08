fb518da89e204f5ad9cb1dd298322b78d01ef3be
Ahdail Netto <ahdail@gmail.com>
2017-02-08 11:01:15 -0300

    Themes WP

---
+++ themes/zeniwp/partials/loop-main.php
@@ -0,0 +1,9 @@
+<?php if (have_posts()) : while(have_posts()) : the_post();?>
+
+	<?php get_template_part('partials/content', 'default' );?>
+
+<?php endwhile;?>
+
+<?php else;?>
+	<?php get_template_part('partials/content', 'none' );?>
+<?php endif;?>
